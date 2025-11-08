b45766eb195bfe520aeddc79794783592b594e0f
Ahmed <ahmed@lightspeedtech.io>
2025-11-08 23:47:29 +0100

    first

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,22 @@
-import { Hono } from 'hono'
-import { CONSTANT } from '@repo/constants'
+import { Hono } from "hono";
+import { CONSTANT } from "@repo/constants";
+import { database } from "@repo/db";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text(CONSTANT)
-})
+app.get("/create-page", async (c) => {
+  const body = await c.req.json();
+  const { name } = body;
+  const page = await database.page.create({
+    data: {
+      name,
+    },
+  });
+  return c.json(page);
+});
 
-export default app
+app.get("/get-pages", async (c) => {
+  const pages = await database.page.findMany();
+  return c.json(pages);
+});
+export default app;
