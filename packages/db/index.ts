da886e50437cce203939766947d49cdbb9300678
Ahmed <ahmed@lightspeedtech.io>
2025-11-09 00:29:54 +0100

    dfd

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,19 +1,10 @@
+import "server-only";
+
 import { neonConfig } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import ws from "ws";
 import { PrismaClient } from "./generated/client";
-import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
-
-const keys = () =>
-  createEnv({
-    server: {
-      DATABASE_URL: z.url(),
-    },
-    runtimeEnv: {
-      DATABASE_URL: process.env.DATABASE_URL,
-    },
-  });
+import { keys } from "./keys";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
@@ -26,3 +17,5 @@ export const database = globalForPrisma.prisma || new PrismaClient({ adapter });
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = database;
 }
+
+export * from "./generated/client";
