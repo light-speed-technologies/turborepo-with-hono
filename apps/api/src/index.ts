8220db8c041cc9f18cfe733d3a90a28a0c1cd996
Ahmed <ahmed@lightspeedtech.io>
2025-11-09 15:16:55 +0100

    fix: add tsc-alias

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { trpcServer } from "@hono/trpc-server";
-import { createContext } from "./trpc/index.js";
-import { apiRouter } from "./routers/index.js";
+import { createContext } from "./trpc/index";
+import { apiRouter } from "./routers/index";
 import { serve } from "@hono/node-server";
 
 const app = new Hono();
