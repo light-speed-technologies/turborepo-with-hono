6fb5931eacefd0b1dc274fb1b34f5ae7e9ef9c28
Ahmed <ahmed@lightspeedtech.io>
2025-11-09 16:56:05 +0100

    chore: remove vercel workaround (working with .js)

--- apps/api/api/index.ts
+++
@@ -1,7 +0,0 @@
-// This is Vercel's Serverless Function entrypoint
-
-// Assume tsup has been run and the output is in dist/index.js
-// (This is required because vercel doesn't play well with internal npm packages, so we must manually bundle)
-// @ts-ignore - Compiled output doesn't have type declarations
-
-export { default } from "../dist/index.js";
